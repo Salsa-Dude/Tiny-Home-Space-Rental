f42ed8240d96dcdb7d19974960d53c62c7f44ab6
Joseph Arias <arias.joseph.a@gmail.com>
2018-12-21 22:43:22 -0500

    created Review model and migration

---
+++ back-end/db/migrate/20181222032957_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.text :review_content
+      t.integer :rating
+      t.belongs_to :reviewer, class_name: "User"
+      t.belongs_to :reviewee, class_name: "User"
+      t.timestamps
+    end
+  end
+end
