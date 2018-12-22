ee4c9994150b7f231a8851a2053b79a3a5f480ea
Joseph Arias <arias.joseph.a@gmail.com>
2018-12-21 22:27:24 -0500

    created Message model and migration

---
+++ back-end/db/migrate/20181222031936_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.string :subject
+      t.text :content
+      t.column :status, :integer, default: 0
+      t.belongs_to :sender, class_name: "User"
+      t.belongs_to :recipient, class_name: "User"
+      t.timestamps
+    end
+  end
+end
