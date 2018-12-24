85b2a5aebd449b36ca4ec460a55f49d11ca7fbb5
Joseph Arias <arias.joseph.a@gmail.com>
2018-12-24 13:49:45 -0500

    Integraded token Authentication for Login and Logout

--- front-end/src/components/LoginForm.js
+++ front-end/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Checkbox, Form, Message } from 'semantic-ui-react'
+import { Button, Form, Message } from 'semantic-ui-react'
 
 
 class LoginForm extends Component {
