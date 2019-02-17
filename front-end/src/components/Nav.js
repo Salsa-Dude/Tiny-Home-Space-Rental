6ffcf1e9b6b7296cec35a90e9b18dee3f49778a0
Joseph Arias <arias.joseph.a@gmail.com>
2019-02-16 20:15:13 -0500

    change nav to class component

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,60 +1,68 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import '../nav.css'
 
-const Nav = ({ location: { pathname }, logged_in, setCurrentUser }) => {
-  let logout =  () => {
-    setCurrentUser(null)
-    localStorage.clear()
-  }
-  return (
-    <Menu pointing secondary size="huge">
-      {logged_in ? (
-        <Fragment>
-          <Menu.Item
-            as={NavLink}
-            to="/profile"
-            name="Home"
-            active={pathname === "/profile"}
-          />
-          <Menu.Item
-            name='Trips'
-            as={NavLink}
-            to="/trips"
-            active={pathname === "/trips"}
-            
-            // onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='My Properties'
-            as={NavLink}
-            to="/myProperties"
-            active={pathname === "/myProperties"}
-            // onClick={this.handleItemClick}
-          />
+class Nav extends Component {
+
+  
+
+  render() {
+
+    let logout =  () => {
+      // setCurrentUser(null)
+      localStorage.clear()
+    }
+    return (
+      <Menu pointing secondary size="huge">
+        {/* {logged_in( */}
+          <Fragment>
+            <Menu.Item
+              as={NavLink}
+              to="/profile"
+              name="Home"
+              // active={pathname === "/profile"}
+            />
+            <Menu.Item
+              name='Trips'
+              as={NavLink}
+              to="/trips"
+              // active={pathname === "/trips"}
+              
+              // onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='My Properties'
+              as={NavLink}
+              to="/myProperties"
+              // active={pathname === "/myProperties"}
+              // onClick={this.handleItemClick}
+            />
+          
+            <Menu.Menu position="right">
+            <Menu.Item
+              name='Inbox'
+              as={NavLink}
+              to="/messages"
+              // active={pathname === "/messages"}
+            />
+              <Menu.Item to="/logout" name="Logout" onClick={logout} />
+            </Menu.Menu>
+          </Fragment>
         
-          <Menu.Menu position="right">
           <Menu.Item
-            name='Inbox'
-            as={NavLink}
-            to="/messages"
-            active={pathname === "/messages"}
+            // as={NavLink}
+            // to="/login"
+            name="Login"
+            onClick={this.handleOpen}
+            // active={pathname === "/login"}
           />
-            <Menu.Item to="/logout" name="Logout" onClick={logout} />
-          </Menu.Menu>
-        </Fragment>
-      ) : (
-        <Menu.Item
-          as={NavLink}
-          to="/login"
-          name="Login"
-          active={pathname === "/login"}
-        />
-      )}
-    </Menu>
-  );
-};
+      </Menu>
+    )
+  }
+
+}
+ 
 
 export default withRouter(Nav);
 
