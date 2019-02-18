837551bb42a4e459a12c769b754eb8b44f3bd0b9
Joseph Arias <arias.joseph.a@gmail.com>
2019-02-17 21:11:50 -0500

    Setup modal for update in PropertyCard

--- front-end/src/components/MyPropertyCard.js
+++ front-end/src/components/MyPropertyCard.js
@@ -9,6 +9,8 @@ class MyPropertyCard extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      open: false,
+      modalOpen: false,
       propertyName: this.props.property.name,
       propertyAddress: this.props.property.address,
       propertyCity: this.props.property.city,
@@ -17,10 +19,71 @@ class MyPropertyCard extends Component {
       propertyNotes: this.props.property.notes,
       propertyPrice: this.props.property.price
     }
-
   }
 
+  nameChange = (e) => {
+    this.setState({
+     propertyName: e.target.value
+    })
+  }
+
+  addressChange = (e) => {
+    this.setState({
+     propertyAddress: e.target.value
+    })
+  }
+
+  descriptionChange = (e) => {
+    this.setState({
+     propertyDescription: e.target.value
+    })
+  }
+
+  perkChange = (e) => {
+    this.setState({
+     propertyPerks: e.target.value
+    })
+  }
+
+  noteChange = (e) => {
+    this.setState({
+     propertyNotes: e.target.value
+    })
+  }
+
+   priceChange = (e) => {
+    this.setState({
+     propertyPrice: e.target.value
+    })
+  }
+
+  handleOpen = (e, dimmer) => {
+    this.setState({ dimmer, open: true })
+  }
+
+  handleTwoOpen = () => {
+    this.setState({ modalOpen: true })
+  }
+
+  close = () => {
+    this.setState({ 
+      open: false,
+      propertyName: this.props.property.name,
+      propertyAddress: this.props.property.address,
+      propertyCity: this.props.property.city,
+      propertyDescription: this.props.property.description,
+      propertyPerks: this.props.property.perks,
+      propertyNotes: this.props.property.notes,
+      propertyPrice: this.props.property.price
+    })
+  }
+  
+  handleClose = () => this.setState({ modalOpen: false })
+
   render() {
+
+    const { open, dimmer } = this.state
+
     return (
       <Fragment>
         <Item>
@@ -36,7 +99,54 @@ class MyPropertyCard extends Component {
               <p>Price: <span class="property-form-data">${this.props.property.price}</span></p>
             </Item.Description>
             <div className="property-buttons">
-              <Button className="edit-btn" size="small" > <Icon link name='edit' />Edit</Button>
+              <Button onClick={(e) => this.handleOpen(e, 'default' )} className="edit-btn" size="small" > <Icon link name='edit' />Edit</Button>
+              <Modal size="tiny" dimmer={dimmer} open={open} onClose={this.close}>
+            <Header>Update Property </Header>
+            <Modal.Content className="">
+              <Modal.Description className="">
+                <Form>
+                  <Form.Field>
+                    <label>Name</label>
+                    <input name="propertyName" onChange={(e) => this.nameChange(e)}  value={this.state.propertyName} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Address</label>
+                    <input name="propertyAddress" onChange={(e) => this.addressChange(e)} value={this.state.propertyAddress} />
+                  </Form.Field>
+                  <Form.Field>
+                  <Form.TextArea onChange={(e) => this.descriptionChange(e)} name="propertyDescription" rows='3' label='Description' value={this.state.propertyDescription}  />
+                  <Form.TextArea onChange={(e) => this.perkChange(e)} name="propertyPerk" rows='2' value={this.state.propertyPerks} label='Perks' />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Notes</label>
+                    <input name="propertyNotes" onChange={(e) => this.noteChange(e)} value={this.state.propertyNotes} />
+                  </Form.Field>
+                  <Form.Field>
+                    <label>Daily Price</label>
+                    <input name="propertyPrice" onChange={(e) => this.priceChange(e)} value={this.state.propertyPrice} />
+                  </Form.Field>
+                </Form>
+              </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color='black' onClick={this.close}>
+                Cancel
+              </Button>
+              <Button
+                positive
+                icon='checkmark'
+                labelPosition='right'
+                content="Update Course"
+                onClick={this.updateCourse}
+              />
+            </Modal.Actions>
+          </Modal>
+              
+              
+              
+              
+              
+              
               <Button className="delete-btn" size="small" > <Icon link name='trash' />Delete</Button>
             </div>
           </Item.Content>
